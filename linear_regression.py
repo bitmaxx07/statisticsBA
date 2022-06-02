885d49c8e053bc5f94249783ad4d5ede7924fdf9
Xun Ma <bitmaxx07@gmail.com>
2022-06-02 20:55:59 +0200

    configured

--- linear_regression.py
+++ linear_regression.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sklearn.linear_model
 
 data = pd.read_excel("arellano_data.xlsx")
+data.to_csv("res.csv")
 
 # correlation from here
 
@@ -11,13 +12,13 @@ print("Correlation coefficient between educ and skills: " + str(df["skills"].cor
 
 ######################
 
-# linear regression from here
+# linear regression education and large firm
 # read x and y data
 print("largefirm regression: ")
 x = data.educ.values
 y = data.largefirm.values
 
-length = 4820
+length = len(df.index)
 x = x.reshape(length, 1)
 y = y.reshape(length, 1)
 
@@ -34,11 +35,29 @@ res = "Regression result: y = " + str(regr.coef_) + "x + " + str(regr.intercept_
 print(res)
 
 ########################
+# linear regression education, skills and large firm
+print("regression education & skills vs. large firm")
+x_edu_skills = df[['educ', 'skills']]
+# x_edu_skills = x_edu_skills.reshape(length, 1)
+y_edu_skills = df['largefirm']
+
+regr_edu_skills = sklearn.linear_model.LinearRegression()
+regr_edu_skills.fit(x_edu_skills, y_edu_skills)
+# plt.scatter(x_edu_skills, y_edu_skills, color="red")
+# plt.plot(x_edu_skills, regr_edu_skills.predict(x_edu_skills), color="yellow", linewidth=3)
+# plt.xticks()
+# plt.xlabel("edu & skills")
+# plt.yticks()
+# plt.ylabel("large firm")
+# plt.show()
+res_edu_skills = "Regression result: y = " + str(regr_edu_skills.coef_) + "x + " + str(regr_edu_skills.intercept_)
+print(res_edu_skills)
+
 
 # linear regression for workers in Europe
 print("workers regression: ")
 df_new = df.loc[df['europe'] == 1]
-y_eu = df_new.skills.values
+y_eu = df_new.largefirm.values
 x_eu = df_new.educ.values
 len_eu = len(df_new.index)
 
